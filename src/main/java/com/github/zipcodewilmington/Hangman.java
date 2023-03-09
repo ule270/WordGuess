b83f7f20f82bd76a9e286a04569432b6a523f35f
ule270 <u.le270@gmail.com>
2023-03-08 22:40:35 -0500

    Added changes.

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,4 +1,6 @@
 package com.github.zipcodewilmington;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,39 +10,54 @@ import java.util.Scanner;
  * @date 3.6.23
  */
 public class Hangman {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String[] words = {"sushi", "fries", "tacos", "chips", "pizza"};
 
-        int attempts = 10;
+        int attempts = 5;
         int lengthWord;
-        boolean solved;
         Scanner keyboard;
         String guess;
+        String chosen = "";
+        String[] currentG = new String[]{"_", "_","_","_","_"};
 
         System.out.println("Welcome to Hangman where the category is Takeout Foods");
-//        getting random index
-//        Random rand = new Random();
-//        Integer correct = rand.nextInt(4);
+//        getting random index and chooses words from word bank
+        Random rand = new Random();
+        Integer chosenIndex = rand.nextInt(attempts - 1);
+        chosen = words[chosenIndex];
 
 //      getting input guess
         keyboard = new Scanner(System.in);
-        System.out.println("Please enter a letter. You will have 8 attempts");
+        System.out.println("Please enter a character. You will have 5 attempts.");
         guess = keyboard.next();
 
-        for (int i =1; i<=5; i++){
-//            word length
-            for (int j =0; j < 5; j++) {
-
-
+        while (attempts <= 5 && attempts > 0) {
+            for (int i = 0; i < chosen.length(); i++) {
+                char currentChar = chosen.charAt(i);
+                char guessChar = guess.charAt(0);
+                if (currentChar == guessChar) {
+                    currentG[i] = guess;
+                }
             }
-//            attempts left
-            while (attempts <=10 && attempts > 0) {
-                System.out.println("\nAttempts left: " + attempts);
-                System.out.print("Enter letter: ");
-
-                attempts--;
+            for (String currentString : currentG) {
+                System.out.print(currentString);
+            }
+            System.out.println("\nAttempts left: " + attempts);
+            System.out.print("Enter character: ");
+            guess = keyboard.next();
+            attempts--;
+        }
+        ArrayList<String> currentList = (ArrayList<String>) Arrays.asList(currentG);
+           if (currentList.contains("_")) {
+            System.out.print("Incorrect guesses, all attempts used. Here are your guesses.");
+            for (String currentString : currentG) {
+                System.out.print(currentString);
+            }
+        } else {
+            System.out.print("Good job! You guessed correctly!");
+            for (String currentString : currentG) {
+                System.out.print(currentString);
             }
         }
-
     }
 }
